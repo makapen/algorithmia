2d7935b7c5bdcd837655cf1aa03e8cc153ad3360
Tyler Iguchi <tyleriguchi@Tylers-MacBook-Pro.local>
2015-09-03 13:40:14 -0700

    add bootstrap jqueryg

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -25,7 +25,8 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
-
+  app.import('bower_components/bootstrap-sass/assets/javascripts/bootstrap.min.js');
+  
   var glyphFontTree = new Funnel('bower_components/bootstrap-sass/assets/fonts/bootstrap', {
     srcDir: '/',
     destDir: 'fonts/bootstrap'
