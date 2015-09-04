4480858542f818a9bb059d9020e930ef51445b78
clairelyles <claire.lyles@gmail.com>
2015-09-04 11:00:06 -0700

    added more fields to ajax job post

--- app/controllers/post-content.js
+++ app/controllers/post-content.js
@@ -8,7 +8,15 @@ actions: {
     ajax({
       method: "POST",
       url: "http://elasticsearch-makapen.rhcloud.com/content/job",
-      data: JSON.stringify({ hello: "John", location: "Boston", '@timestamp': "2015-05-23" }),
+      data: JSON.stringify({
+        jobTitle: "Technical Project Manager",
+        company: "Wallero Technologies",
+        description: "You will manage programs/projects involving cross-functional teams focused on the delivery of technical products or systems.",
+        city: "Seattle",
+        state: "WA",
+        companySize: "1-50",
+        '@timestamp': "2015-05-23"
+      }),
       'content-type': 'application/json'
     })
       .then(function( msg ) {
