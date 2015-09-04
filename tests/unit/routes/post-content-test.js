c3d5c29db201777fbb994847c3045d61914e2f6e
clairelyles <claire.lyles@gmail.com>
2015-09-03 20:11:14 -0700

    post content page up

---
+++ tests/unit/routes/post-content-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:post-content', 'Unit | Route | post content', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  var route = this.subject();
+  assert.ok(route);
+});
