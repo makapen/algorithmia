355f36f032fabe66ac0455978eed48631432ba73
tyleriguchi <tyleriguchi@gmail.com>
2015-09-04 15:30:35 -0700

    use job model, created date

--- app/controllers/post-content.js
+++ app/controllers/post-content.js
@@ -2,22 +2,16 @@ import Ember from 'ember';
 import ajax from 'ic-ajax';
 
 export default Ember.Controller.extend({
-actions: {
-
-  submitJobPost: function () {
-    ajax({
-      method: "POST",
-      url: "http://elasticsearch-makapen.rhcloud.com/content/job",
-      data: JSON.stringify({ hello: "John", location: "Boston", '@timestamp': "2015-05-23" }),
-      'content-type': 'application/json'
-    })
-      .then(function( msg ) {
-        console.log('post to kibana successful!');
-      });
+  actions: {
+    submitJobPost() {
+      this.store.createRecord('job', {
+        title: this.get('title'),
+        company: this.get('company'),
+        companySize: this.get('companySize'),
+        city: this.get('city'),
+        state: this.get('state'),
+        description: this.get('description')
+      }).save();
+    }
   }
-
-}
-
-
-
 });
