b1f3973dd26b13908214514e244f8385938cc38c
tyleriguchi <tyleriguchi@gmail.com>
2015-09-04 17:49:43 -0700

    fix company size

--- app/helpers/time-from-date.js
+++ app/helpers/time-from-date.js
@@ -3,7 +3,6 @@ import Ember from 'ember';
 export function timeFromDate(params/*, hash*/) {
   let str = params[0];
   let time = str.split('.')[0];
-  console.log('time', time)
   return moment(time).fromNow();
 }
 
