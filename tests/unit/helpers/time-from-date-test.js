0860172566543a8053d3f9f80997fa2abbd3e5da
tyleriguchi <tyleriguchi@gmail.com>
2015-09-04 17:21:56 -0700

    list posted jobs, show time posted

---
+++ tests/unit/helpers/time-from-date-test.js
@@ -0,0 +1,10 @@
+import { timeFromDate } from '../../../helpers/time-from-date';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | time from date');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  var result = timeFromDate(42);
+  assert.ok(result);
+});
