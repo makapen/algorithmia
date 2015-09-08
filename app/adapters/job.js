8e3f1e0bf8de383a8d74b2d69720bf387b40f745
clairelyles <claire.lyles@gmail.com>
2015-09-08 15:41:59 -0700

    added job preview page

--- app/adapters/job.js
+++ app/adapters/job.js
@@ -2,14 +2,37 @@ import DS from 'ember-data';
 import ajax from 'ic-ajax';
 
 export default DS.RESTAdapter.extend({
-  host: "http://elasticsearch-makapen.rhcloud.com/content",
+  host: "https://elasticsearch-makapen.rhcloud.com/content",
   pathForType() {
     return 'job';
   },
   findAll: function(store, type) {
-    var url = type;
     return ajax({
       url: this.get('host') + '/job/_search',
+      method: 'get',
+      dataType: 'json',
+      data: JSON.stringify({
+        "query" : {
+          "match_all" : {}
+        }
+      })
+    }).then( (res) => {
+      var results = res.hits.hits;
+      var resultObjects = results.map( (item) => {
+        let obj = item._source.job;
+        obj.id = item._id;
+        return obj;
+      });
+      return {
+        jobs: resultObjects
+      }
+    }).catch( (err) => {
+      return err;
+    })
+  },
+  findRecord: function(store, type, id) {
+    return ajax({
+      url: this.get('host') + '/job/_search/?q=_id:' + id,
       method: 'get',
       dataType: 'json',
       data: JSON.stringify({
--- app/router.js
+++ app/router.js
@@ -9,6 +9,7 @@ Router.map(function() {
   this.route('jobs');
   this.route('user');
   this.route('post-content');
+  this.route('job', { path: 'job/:id' });
 });
 
 export default Router;
