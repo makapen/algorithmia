587248072e7dc694727b77bceaf6e895849f8d00
tyleriguchi <tyleriguchi@gmail.com>
2015-09-09 12:16:29 -0700

    re add stuff

--- app/controllers/posts.js
+++ app/controllers/posts.js
@@ -3,5 +3,21 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   currentJob: function() {
     return this.get('session.secure.profile.positions.values.firstObject');
-  }.property('session.secure.profile.positions.values.[]')
+  }.property('session.secure.profile.positions.values.[]'),
+
+  displayPosts: Ember.computed('posts.[]', function() {
+    return this.get('posts');
+  }),
+
+  actions: {
+    submitSearch: function() {
+      this.store.query('post', {
+        "title": this.get('query')
+      }).then( (posts) => {
+        this.set('posts', posts);
+      }).catch( (err) => {
+        throw new Error(err);
+      })
+    }
+  }
 });
--- app/routes/posts.js
+++ app/routes/posts.js
@@ -3,5 +3,9 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('post');
+  },
+  setupController(controller, model) {
+    this._super(controller, model);
+    controller.set('posts', model);
   }
 });
