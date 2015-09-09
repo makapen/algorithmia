33851c77865a2a8c8897a5e7578e63d9062fd18f
tyleriguchi <tyleriguchi@gmail.com>
2015-09-09 13:16:55 -0700

    sort by most recent

--- app/routes/posts.js
+++ app/routes/posts.js
@@ -2,7 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('post');
+    return this.store.findAll('post').then( (posts) => {
+      return posts.get('content').mapBy('record').sort(function(a, b) {
+        Ember.Logger.log('item', a);
+        return moment(a.get('createdDate')).isBefore(moment(b.get('createdDate')));
+      });
+    })
   },
   setupController(controller, model) {
     this._super(controller, model);
