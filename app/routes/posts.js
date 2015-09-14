9de8f32735317bb716da9ea951d12c5e18f0b012
tyleriguchi <tyleriguchi@gmail.com>
2015-09-14 15:52:45 -0700

    refactor posts to use query params

--- app/routes/posts.js
+++ app/routes/posts.js
@@ -1,15 +1,30 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model() {
-    return this.store.findAll('post').then( (posts) => {
-      return posts.get('content').mapBy('record').sort(function(a, b) {
-        return moment(a.get('createdDate')).isBefore(moment(b.get('createdDate')));
-      });
-    })
+  model(params) {
+    // if there are query params use those
+    let hasQueryParams = params.search ? true : false;
+
+    if (hasQueryParams) {
+      return this.store.query('post', {
+        "title": params.search
+      }).then( (res) => {
+        Ember.Logger.log('query', res)
+        return res;
+      })
+    }
+    else {
+      return this.store.findAll('post').then( (posts) => {
+        return posts.get('content').mapBy('record').sort(function(a, b) {
+          return moment(a.get('createdDate')).isBefore(moment(b.get('createdDate')));
+        });
+      })
+    }
   },
-  setupController(controller, model) {
-    this._super(controller, model);
-    controller.set('posts', model);
+
+  actions: {
+    reloadPosts() {
+      this.refresh();
+    }
   }
 });
