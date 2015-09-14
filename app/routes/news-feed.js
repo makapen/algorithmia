f04dcb901c1c99675c37f60a23eac45aea7992d0
clairelyles <claire.lyles@gmail.com>
2015-09-14 15:17:44 -0700

    news feed model working

--- app/routes/news-feed.js
+++ app/routes/news-feed.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model () {
+    return this.store.findAll('news-feed')
+  },
+  setupController(controller, model) {
+    this._super(controller, model);
+    controller.set('news-feeds', model);
+  }
 });
