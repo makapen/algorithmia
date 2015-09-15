44c3c281d2fe3c77e4559d18fbd5c28383653dea
tyleriguchi <tyleriguchi@gmail.com>
2015-09-14 17:07:55 -0700

    post updates

--- app/controllers/news-feed.js
+++ app/controllers/news-feed.js
@@ -1,7 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  // displayNewsFeed: Ember.computed('news-feed.[]', function() {
-  //   return this.get('news-feed');
-  // })
+  actions: {
+    submitNewsFeed() {
+      return this.store.createRecord('news-feed', {
+        firstName: this.get('session.content.secure.profile.given_name'),
+        lastName: this.get('session.content.secure.profile.family_name'),
+        title: this.get('title'),
+        comment: this.get('comment'),
+        timestamp: Date.now(),
+      }).save();
+    }
+  }
 });
