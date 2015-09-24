56f3abfca70d33f4679f5237ee86732590c2b86f
tyleriguchi <tyleriguchi@gmail.com>
2015-09-23 20:41:58 -0700

    make request for news feed to get sentiment value

---
+++ app/components/load-sentiment.js
@@ -0,0 +1,29 @@
+import Ember from 'ember';
+import ajax from 'ic-ajax';
+
+export default Ember.Component.extend({
+  model: null,
+  loadingContent: true,
+
+  _onAttrs: Ember.on('didInitAttrs', function() {
+    let model = this.get('model');
+    const comment = model.get('comment');
+
+    if (comment && !model.get('sentiment')) {
+      ajax({
+        url: 'http://localhost:3000/sentiment',
+        method: 'POST',
+        data: comment
+      }).then ( (sentimentValue) => {
+        console.log('val', sentimentValue)
+        model.set('sentiment', sentimentValue.result);
+        model.save().then( () => {
+          this.set('loadingContent', false);
+        });
+      }).catch( (err) => {
+        throw new Error(err);
+      })
+    }
+    this.set('loadingContent', false);
+  })
+});
