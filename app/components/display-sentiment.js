ff7c92a02f5bc1242e4d4499cac3467212568c32
clairelyles <claire.lyles@gmail.com>
2015-10-01 14:35:40 -0700

    changed sentiment range

--- app/components/display-sentiment.js
+++ app/components/display-sentiment.js
@@ -3,7 +3,8 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   classNames: ['display-sentiment'],
   value: null,
-  displaySmile: Ember.computed.gt('value', '2'),
+  displaySmile: Ember.computed.gte('value', '3'),
   displayNeutral: Ember.computed.equal('value', '2'),
-  displaySad: Ember.computed.lt('value', '2'),
+  displayNeutral: Ember.computed.equal('value', '1'),
+  displaySad: Ember.computed.equal('value', '0'),
 });
