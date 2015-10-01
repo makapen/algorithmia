8921e2528009da1f1514fd620179c9b1c40592a7
clairelyles <claire.lyles@gmail.com>
2015-10-01 15:15:25 -0700

    changed sentiment scores

--- app/components/display-sentiment.js
+++ app/components/display-sentiment.js
@@ -4,7 +4,6 @@ export default Ember.Component.extend({
   classNames: ['display-sentiment'],
   value: null,
   displaySmile: Ember.computed.gte('value', '3'),
-  displayNeutral: Ember.computed.equal('value', '2'),
-  displayNeutral: Ember.computed.equal('value', '1'),
+  displayNeutral: Ember.computed.gte('value', '1'),
   displaySad: Ember.computed.equal('value', '0'),
 });
